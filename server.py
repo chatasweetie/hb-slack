1a4947338669cf31f0e6f3eef38b76dc0e3fa7d4
Jessica <jessica.dene.earley@gmail.com>
2016-08-02 14:16:38 -0700

    boil plate code

---
+++ server.py
@@ -0,0 +1,32 @@
+"""Slack Slash Commands for Hackbright"""
+
+import os
+
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+# Required t,l.o use Flask sessions and the debug toolbar
+app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "abcdef")
+
+
+@app.route("/")
+def index():
+    """Homepage Test"""
+
+    return "Hello"
+
+if __name__ == "__main__":
+
+    app.debug = False
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+    DEBUG = "NO_DEBUG" not in os.environ
+    PORT = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=PORT, debug=DEBUG)
